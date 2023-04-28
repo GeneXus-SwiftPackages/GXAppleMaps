0eec2a4607a79389312865d1514f38c5223409a9
genexusbot <builder@genexus.com>
2023-04-28 12:42:52 -0300

    Package version: 1.0.0-beta+20230428153652

--- Package.swift
+++ Package.swift
@@ -19,14 +19,14 @@ let package = Package(
 				dependencies: [
 					"GXAppleMaps",
 					.product(name: "GXCoreModule_Common_Geography", package: "GXCoreModule_Common_Maps", condition: .when(platforms: [.iOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS])),
 					.product(name: "GXUCMaps", package: "GXUCMaps", condition: .when(platforms: [.tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAppleMaps",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMaps-1.0.0-beta+20230428131619.xcframework.zip",
-			checksum: "b727aa667d41cc39f75a4b46da334388878160fe6eb04024e4d4df421eab698c"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMaps-1.0.0-beta+20230428153652.xcframework.zip",
+			checksum: "9b488ce2b63d70c1d79b6da29d710dc7ce93793eac5cc85a49e3c9a1386a6c9d"
 		)
 	]
 )

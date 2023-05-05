2b9f038d2f37b49b6d486e0a16e5123ae2379327
Build Er <builder@macmini6.local>
2023-05-05 12:02:24 -0300

    Package version: 1.0.0-beta+20230505145632

--- Package.swift
+++ Package.swift
@@ -19,14 +19,14 @@ let package = Package(
 				dependencies: [
 					"GXAppleMaps",
 					.product(name: "GXCoreModule_Common_Maps", package: "GXCoreModule_Common_Maps", condition: .when(platforms: [.iOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS])),
 					.product(name: "GXUCMaps", package: "GXUCMaps", condition: .when(platforms: [.tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAppleMaps",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMaps-1.0.0-beta+20230505132240.xcframework.zip",
-			checksum: "803d76752f29077ccf169f93b00106ebced663664e3236229d7dec478b7069a0"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMaps-1.0.0-beta+20230505145632.xcframework.zip",
+			checksum: "0d59be76c2d87fedcb9f57e85ac1827cdd71d817c534822301bc75e19af4f5f3"
 		)
 	]
 )

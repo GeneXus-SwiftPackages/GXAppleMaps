edd5ba76cf13e31321c5c072d0044a3a4d4717fd
Build Er <builder@macmini6.local>
2024-04-12 16:23:34 -0300

    Package version: 1.6.0-beta.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXAppleMapsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git", exact: "1.6.0-beta.2"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.6.0-beta.2")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git", exact: "1.6.0-beta.3"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.6.0-beta.3")
 	],
 	targets: [
 		.target(name: "GXAppleMapsWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAppleMaps",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMaps-1.6.0-beta.2.xcframework.zip",
-			checksum: "1d7aa9eea5195468f353c6056ae2055555c8188123b16ca21fa9377aa6c307ad"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMaps-1.6.0-beta.3.xcframework.zip",
+			checksum: "7fde9e7dfb8113ae2597abf843c319700e174755ae0578fa3f9184723ddd0d09"
 		)
 	]
 )

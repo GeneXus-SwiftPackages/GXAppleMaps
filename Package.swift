17f9cd11c86c8d4e8ef8d13d97ab4f028c3b1c92
Build Er <builder@macmini6.local>
2024-05-13 21:42:07 -0300

    Package version: 2.0.0-beta.31

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXAppleMapsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git", exact: "2.0.0-beta.30"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.30")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreModule_Common_Maps.git", exact: "2.0.0-beta.31"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.31")
 	],
 	targets: [
 		.target(name: "GXAppleMapsWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAppleMaps",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMaps-2.0.0-beta.30.xcframework.zip",
-			checksum: "9c542db676988572cc3be466c08ce72632b0b27e1a7476745b0a25d27b7e2f8a"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMaps-2.0.0-beta.31.xcframework.zip",
+			checksum: "22aa5455cae6e679444ee8892266a06f7faf7c4f24d2e020ac43fa4ebc368b99"
 		)
 	]
 )
